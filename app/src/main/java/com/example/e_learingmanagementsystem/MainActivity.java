794fb237c48df57de93e6be7226489e906a5e755
ReshmaRajPuthenpurayilRajappan <puthenpurayilrajappanr@student.douglascollege.ca>
2021-07-22 20:47:03 -0700

    Updated navigation drawer activity.

--- app/src/main/java/com/example/e_learingmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/e_learingmanagementsystem/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.e_learingmanagementsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.e_learingmanagementsystem.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding=null;
     }
 }
