12d49c9f4d1287478c52d824f3c2dc9849231c1b
ReshmaRajPuthenpurayilRajappan <puthenpurayilrajappanr@student.douglascollege.ca>
2021-08-04 12:26:47 -0700

    Updated login and signup activities

--- app/src/main/java/com/example/e_learingmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/e_learingmanagementsystem/MainActivity.java
@@ -2,34 +2,31 @@ package com.example.e_learingmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.example.e_learingmanagementsystem.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    TextView createNewAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNewAccount = findViewById(R.id.textViewCreateNewAccount);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+        createNewAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        binding=null;
     }
 }
--- app/src/main/java/com/example/e_learingmanagementsystem/RegisterActivity.java
+++ app/src/main/java/com/example/e_learingmanagementsystem/RegisterActivity.java
@@ -2,13 +2,25 @@ package com.example.e_learingmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView alreadyHaveAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        alreadyHaveAccount=findViewById(R.id.textViewAlreadyHaveAccount);
+
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
     }
 }
